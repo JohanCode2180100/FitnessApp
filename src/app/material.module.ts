4f3afa2e3da2dae565c65c59acda35ba303b0f5f
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-09-25 21:49:14 +0200

    add component for started app (login, signup, trainin, new-training, post-training) and add AppRoutingModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
